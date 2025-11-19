b7afbd40e61a52733ba8b4e8c46d7eb2034f4680
szilard <szilard@users.noreply.github.com>
2025-11-19 17:10:42 +0000

    Refactor benchmark script output for clarity and consistency

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+echo "=========================================="
+echo "Generating test data..."
+echo "=========================================="
+Rscript gendata.R
+echo ""
+
+echo "=========================================="
+echo "Runnning the benchmark..."
+echo "=========================================="
+echo ""
+
 # R Benchmarks
-echo "==================== data.table ===================="
+echo "-------------------- data.table --------------------"
 Rscript tools/data_table.R
 echo ""
 
-echo "==================== dplyr ===================="
+echo "-------------------- dplyr --------------------"
 Rscript tools/dplyr.R
 echo ""
 
-echo "==================== DuckDB (R) ===================="
-Rscript tools/duckdb.R
+echo "-------------------- DuckDB --------------------"
+python3 tools/duckdb_df.py
 echo ""
 
 # Python Benchmarks
-echo "==================== pandas ===================="
+echo "-------------------- pandas --------------------"
 python3 tools/pandas_df.py
 echo ""
 
-echo "==================== polars ===================="
+echo "-------------------- polars --------------------"
 python3 tools/polars_df.py
 echo ""
